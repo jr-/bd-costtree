78c2dec2dc7f5c9dc982537f7198a4f75cf5379a
nicolas0p <nicolas0pfeifer@gmail.com>
2017-06-05 14:31:07 -0300

    Corrected Table attributes and the constructor. Created methods to populate the table

--- include/OperatorNode.hpp
+++ include/OperatorNode.hpp
@@ -3,9 +3,11 @@
 
 #include <string>
 #include <deque>
+#include <unordered_map>
 
 using std::string;
 using std::deque;
+using std::unordered_map;
 
 enum type {
 	STRING, INT
@@ -36,33 +38,44 @@ class OperatorNode {
 
 class Table : public OperatorNode {
 	public:
-		Table(string name, deque<std::tuple<string, type, int>> attributes, deque<string> primary_key, deque<std::tuple<string, string>> foreign_keys, std::pair<int, int>* _primary_index, deque<std::tuple<string, int, int>> secondary_index, int tuple_quantity);
+		Table(string name, int tuple_quantity);
 		virtual ~Table();
 
-		Table result();
-		int best_access_number();
+		Table result() const;
+		int best_access_number() const;
+
+		//must be unique
+		void add_attribute(string attribute_name, type attribute_type, int size, int variability);
+		//must be valid attributes already in the table
+		void add_primary_key(deque<string> primary_key);
+		void add_foreign_key(string attribute_name, string foreign_table_name);
+		void add_secondary_index(string attribute_name, int n, int fi);
+		void add_primary_index(int n, int fi);
+		void ordered_by(string attribute);
 
 	private:
 		string _name;
-		//attribute name, type and size in bytes
-		deque<std::tuple<string, type, int>> _attributes;
+		//attribute name indexes type, size in bytes and variability
+		unordered_map<string, std::tuple<type, int, int>> _attributes;
 		//must contain members of attributes
-		deque<string>_primary_key;
-		//foreign keys<attribute name, table name>
-		deque<std::tuple<string, string>> _foreign_keys;
-		//Does this table have a primary index? What is its N and Fi?
-		std::pair<int, int>* _primary_index_values;
-		//secondary index<name, N, Fi>
-		deque<std::tuple<string, int, int>> _secondary_index;
+		deque<string> _primary_key;
 		//tuple quantity
 		int _tuple_quantity;
+		//foreign keys. attribute name indexes table name
+		unordered_map<string, string> _foreign_keys;
+		//Does this table have a primary index? What is its N and Fi? If not, <0,0>
+		std::pair<int, int> _primary_index;
+		//secondary index<name, N, Fi>
+		unordered_map<string, std::pair<int, int>> _secondary_indexes;
+		//are the tuples ordered in disk by an attribute? If not, empty string
+		string _ordered_by;
 
 };
 
-class SelectionNode : OperatorNode {
+class SelectionNode : public OperatorNode {
 
 	public:
-		SelectionNode(const OperatorNode* left, const OperatorNode* right, Expression expression);
+		SelectionNode(const OperatorNode* left, Expression expression);
 		virtual ~SelectionNode();
 
 		int best_access_number();
@@ -74,10 +87,10 @@ class SelectionNode : OperatorNode {
 
 };
 
-class ProjectionNode : OperatorNode {
+class ProjectionNode : public OperatorNode {
 
 	public:
-		ProjectionNode(const OperatorNode* left, const OperatorNode* right, deque<string, string> attributes);
+		ProjectionNode(const OperatorNode* left, deque<string, string> attributes);
 		virtual ~ProjectionNode();
 
 		int best_access_number();
@@ -89,7 +102,7 @@ class ProjectionNode : OperatorNode {
 
 };
 
-class ProductNode : OperatorNode {
+class ProductNode : public OperatorNode {
 
 	public:
 		ProductNode(const OperatorNode* left, const OperatorNode* right);
@@ -100,7 +113,7 @@ class ProductNode : OperatorNode {
 
 };
 
-class JoinNode : OperatorNode {
+class JoinNode : public OperatorNode {
 
 	public:
 		JoinNode(const OperatorNode* left, const OperatorNode* right, Expression expression);
@@ -114,7 +127,7 @@ class JoinNode : OperatorNode {
 
 };
 
-class NaturalJoinNode : OperatorNode {
+class NaturalJoinNode : public OperatorNode {
 
 	public:
 		NaturalJoinNode(const OperatorNode* left, const OperatorNode* right);
@@ -125,6 +138,18 @@ class NaturalJoinNode : OperatorNode {
 
 };
 
-//selection, projection, product, join, naturalJoin
+class Database {
+
+	public:
+		Database(int nbuf, int block_size);
+		void insert_table(Table table);
+
+	private:
+		//buffer size
+		int _nBuf;
+		//disk block size in bytes
+		int _block_size;
+		deque<Table> _tables;
+};
 
 #endif  // OPERATOR_NODE_HPP
